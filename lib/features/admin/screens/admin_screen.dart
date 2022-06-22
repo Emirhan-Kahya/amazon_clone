f0050b66c6660d563ac280d63d906afb58c2a9a9
Emirhan <eKahya22@gmail.com>
2022-06-22 12:44:23 +0300

    22/06/2022

---
+++ lib/features/admin/screens/admin_screen.dart
@@ -0,0 +1,132 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/admin/screens/posts_screen.dart';
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+
+  int _pageIndex = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+
+  List<Widget> pages = [
+    const PostsScreen(),
+    const Center(child: Text('Analytics Page'),),
+    const Center(child: Text('Shopping Cart'),),  ];
+
+  void updatePage(int page){
+    setState((){
+      _pageIndex = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          elevation: 0,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: mVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              const Text(
+                'Admin',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        selectedItemColor: mVariables.selectedNavBarColor,
+        unselectedItemColor: mVariables.unselectedNavBarColor,
+        backgroundColor: mVariables.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        items: [
+          //Posts
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _pageIndex == 0
+                        ? mVariables.selectedNavBarColor
+                        : mVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(Icons.home_outlined),
+            ),
+            label: '',
+          ),
+          // Orders
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _pageIndex == 1
+                        ? mVariables.selectedNavBarColor
+                        : mVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(Icons.all_inbox_outlined),
+            ),
+            label: '',
+          ),
+          //Analytics
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _pageIndex == 2
+                        ? mVariables.selectedNavBarColor
+                        : mVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(Icons.analytics_outlined),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
